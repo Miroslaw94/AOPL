a4e2188511e01dc4f49b9bef65fcbca6198962e5
Mariusz Korzekwa <maledorak@gmail.com>
2019-04-17 17:38:30 +0200

    Add view fixes

--- music/tests.py
+++ music/tests.py
@@ -6,5 +6,5 @@ from music.views import home_page
 class HomePageTest(TestCase):
 
     def test_url_resolves_to_home_view(self):
-        found = resolve('')
+        found = resolve('/')
         self.assertEqual(found.func, home_page)
