6a0ec12833b0786b0b148897bedf802e96825aac
Miroslaw94 <miras_94@wp.pl>
2019-04-23 20:47:04 +0200

    Functional tests now checks site with music notes.

--- functional_tests.py
+++ functional_tests.py
@@ -13,7 +13,20 @@ class NewVisitorTest(unittest.TestCase):
     def test_open_site(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('AOPL', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Akademicka Orkiestra', header_text)
         self.fail('Finish the test!')
+
+    def test_music_notes_site(self):
+        self.browser.get('http://localhost:8000/nuty/')
+        self.assertIn('AOPL', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Nuty', header_text)
+
+        button = self.browser.find_element_by_tag_name('h2')
+        self.assertEqual('Dodaj nuty', button.text)
+        self.assertEqual(button.get_attribute('href'), 'http://localhost:8000/nuty/dodaj/')
 
 
 if __name__ == '__main__':
