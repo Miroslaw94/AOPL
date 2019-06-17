14cb4149e4285051a0be5deda184d4b82baff3f8
Miroslaw94 <miras_94@wp.pl>
2019-06-17 17:41:08 +0200

    Added test for editing and deleting music notes.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,8 @@ from django.test import LiveServerTestCase
 import unittest
 import time
 
+from music.models import MusicNotes
+
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -21,8 +23,6 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_music_notes_site(self):
         self.browser.get(self.live_server_url + '/nuty/')
-        self.assertIn('AOPL', self.browser.title)
-
         header_text = self.browser.find_element_by_tag_name('h2').text
         self.assertIn('Nuty', header_text)
 
@@ -33,15 +33,13 @@ class NewVisitorTest(LiveServerTestCase):
 
     def test_add_music_notes(self):
         self.browser.get(self.live_server_url + '/nuty/dodaj/')
-        self.assertIn('AOPL', self.browser.title)
-
         name_input = self.browser.find_element_by_name('title')
         name_input.send_keys('Muppets')
 
         file_input = self.browser.find_element_by_css_selector("input[type='file'][name='viola']")
         file_input.send_keys('/Users/Miroslaw_Siwik/kodzenie/aopl/nuty_testowe.pdf')
         self.browser.find_element_by_name('submit').click()
-        time.sleep(2)
+        time.sleep(1)
 
         posts = self.browser.find_elements_by_class_name('list-group-item')
         self.assertTrue(posts[0].text, 'Muppets')
@@ -49,3 +47,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(posts[0].get_attribute('href'))
         header_text = self.browser.find_element_by_tag_name('h2').text
         self.assertIn('Muppets', header_text)
+
+    def test_delete_music_notes(self):
+        MusicNotes.objects.create(title='Polonez')
+        MusicNotes.objects.create(title='Muppets')
+        MusicNotes.objects.create(title='Waltz')
+
+        self.browser.get(self.live_server_url + '/nuty/')
+        button = self.browser.find_elements_by_class_name('btn-danger')
+        button[0].click()
+        time.sleep(1)
+
+        posts = self.browser.find_elements_by_class_name('list-group-item')
+        self.assertTrue(posts[0].text, 'Muppets')
+
+    def test_edit_music_notes(self):
+        MusicNotes.objects.create(title='Polonez')
+
+        self.browser.get(self.live_server_url + '/nuty/')
+        button = self.browser.find_elements_by_class_name('btn-warning')
+        button[0].click()
+        time.sleep(1)
+
+        name_input = self.browser.find_element_by_name('title')
+        name_input.send_keys('Muppets')
+        self.browser.find_element_by_name('submit').click()
+        time.sleep(1)
+
+        post = self.browser.find_element_by_class_name('list-group-item')
+        self.assertTrue(post.text, 'Muppets')
