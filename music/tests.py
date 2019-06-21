9a15df626ef4cb6b4208364e510f22cab4a3d667
Miroslaw94 <miras_94@wp.pl>
2019-06-21 12:47:45 +0200

    Added unittest for deleting items.

--- music/tests.py
+++ music/tests.py
@@ -1,5 +1,7 @@
+import os
 from django.test import TestCase
 from music.models import MusicNotes
+from aopl.settings import MEDIA_ROOT
 
 
 class PageTest(TestCase):
@@ -17,14 +19,14 @@ class PageTest(TestCase):
         self.assertTemplateUsed(response, 'add_music_notes.html')
 
     def test_can_save_a_POST_request(self):
-        self.client.post('/nuty/dodaj/', data={'title': 'Muppets'})
+        self.client.post('/nuty/dodaj/', data={'title': 'Test_item'})
 
         self.assertEqual(MusicNotes.objects.count(), 1)
         new_notes = MusicNotes.objects.first()
-        self.assertEqual(new_notes.title, 'Muppets')
+        self.assertEqual(new_notes.title, 'Test_item')
 
     def test_redirects_after_post(self):
-        response = self.client.post('/nuty/dodaj/', data={'title': 'Muppets'})
+        response = self.client.post('/nuty/dodaj/', data={'title': 'Test_item'})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/nuty/')
 
@@ -41,16 +43,32 @@ class PageTest(TestCase):
 class MusicNotesModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
-        muppets = MusicNotes()
-        muppets.title = 'Muppets'
-        muppets.viola = '/Users/Miroslaw_Siwik/kodzenie/aopl/nuty_testowe.pdf'
-        muppets.save()
+        item1 = MusicNotes()
+        item1.title = 'item1'
+        item1.viola = '/Users/Miroslaw_Siwik/kodzenie/aopl/nuty_testowe.pdf'
+        item1.save()
 
         saved_notes = MusicNotes.objects.all()
         self.assertEqual(saved_notes.count(), 1)
-        self.assertEqual(saved_notes[0].title, 'Muppets')
+        self.assertEqual(saved_notes[0].title, 'item1')
         self.assertEqual(saved_notes[0].viola, '/Users/Miroslaw_Siwik/kodzenie/aopl/nuty_testowe.pdf')
 
         response = self.client.get('/nuty/1/')
         self.assertTemplateUsed(response, 'music_notes_details.html')
-        self.assertIn('Muppets', response.content.decode())
+        self.assertIn('item1', response.content.decode())
+
+    def test_deleting_items(self):
+        item1 = MusicNotes()
+        item1.title = 'item1'
+        item1.save()
+        item2 = MusicNotes()
+        item2.title = 'item2'
+        item2.save()
+
+        saved_notes = MusicNotes.objects.all()
+        self.assertEqual(saved_notes.count(), 2)
+
+        item1.delete()
+
+        self.assertEqual(saved_notes.count(), 1)
+        self.assertEqual(saved_notes[0].title, 'item2')
